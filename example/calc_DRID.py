e770db26ebc8e818bf3c8a1b7d1131f2d2f85f58
MoSchaeffler <moritz-schaeffler@web.de>
2025-04-16 12:21:07 +0200

    added an example script

---
+++ example/calc_DRID.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from calcDRID import DRID
+
+###############################
+############ Input ############
+###############################
+
+"""
+define topology and trajectory path
+"""
+
+top = '/path/to/my/topology.tpr'
+traj = '/path/to/my/trajectory.xtc'
+
+"""
+define selection for centroids and reference atoms in the MDAnalysis selection style
+    - https://userguide.mdanalysis.org/stable/selections.html
+
+The selection below was used for Abeta42 in https://doi.org/10.1039/D4CC02856B
+"""
+
+sel_cent = "(name CA and resid 28) or (name CA and resid 23) or (name CA and resid 1) or (name CA and resid 42) or (name CA and resid 19) or (name CA and resid 34)"
+sel_atoms = "protein"
+
+"""
+Define name of the output file containing the framewise DRID metric of the system
+saved as npy array
+"""
+
+outname = "DRID_myprotein"
+
+###############################
+############  Run  ############
+###############################
+
+df = DRID(top,traj,sel_atoms,sel_cent)
+df.run(outname)
