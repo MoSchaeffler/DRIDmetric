62803761490665799e42a7007d98ec298e0daff4
MoSchaeffler <moritz-schaeffler@web.de>
2025-09-22 12:48:27 +0200

    update to not include examples if building

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
     version="1.1.0",
     author="Moritz Schaeffler",
     description="Package to calculate the DRID metric from a molecular dynamics trajectory",
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=[
+            "example", "examples", 
+            "example.*", "examples.*",
+        ]
+    ),
+    include_package_data=False,   # do not auto-include non-code files
+    package_data={}, 
     install_requires=["numpy","tqdm","MDAnalysis"],
 )
